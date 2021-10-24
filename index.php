6a242055309c3469294085513a894bfff55d8f13
lln <leengaa37@gmail.com>
2021-10-24 21:45:05 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php include('template-part/header.php') ?>
+
+<?php include('template-part/main-menu.php') ?>
+
+<section id="main">
+  <div class="container">
+    <div class="row g-0">
+      <div class="col-md-8">
+        <div class="grid-post">
+          <div class="col-md-4">
+            <div class="card" style="width: 18rem;">
+              <div class="card-header d-flex justify-content-between align-middle">
+                <div class="user-avatar">
+                  <img src="images/img-author.jpg" width="32">
+                </div>
+                <div class="user-profile">
+                  <div class="u_url-name">
+                    <span>moingay1trangsach.vn</span>
+                  </div>
+                  <div class="u_full-name">
+                    Mỗi Ngày 1 Trang Sách
+                  </div>
+                </div>
+              </div>
+            <img src="images/default-post.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">An item</li>
+              <li class="list-group-item">A second item</li>
+              <li class="list-group-item">A third item</li>
+            </ul>
+            <div class="card-body">
+              <a href="#" class="card-link">Card link</a>
+              <a href="#" class="card-link">Another link</a>
+            </div>
+          </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 ps-5">
+        Info
+      </div>
+    <div>
+  </div>
+</section>
+
+<?php include('template-part/footer.php') ?>
